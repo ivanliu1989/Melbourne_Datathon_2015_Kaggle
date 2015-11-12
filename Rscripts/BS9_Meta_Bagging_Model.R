b32953e312076d476ad247837040fbceb7c1d585
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-11-12 14:41:55 +1100

    meta bagging model

--- Rscripts/BS9_Meta_Bagging_Model.R
+++ Rscripts/BS9_Meta_Bagging_Model.R
@@ -2,47 +2,37 @@ setwd('/Users/ivanliu/Google Drive/Melbourne Datathon/Melbourne_Datathon_2015_Ka
 rm(list=ls()); gc()
 library(xgboost);library(pROC);require(randomForest);library(Rtsne);require(data.table);library(caret)
 load('../S9_train_validation_test_20151110.RData');ls()
-options(scipen=999)
-set.seed(1004)
+options(scipen=999);set.seed(1004)
 
 train <- train
 test <- validation
 train$flag_class <- ifelse(train$flag_class == 'Y', 1, 0)
 test$flag_class <- ifelse(test$flag_class == 'Y', 1, 0)
 feat <- colnames(train)[c(3:(ncol(train)-2))]
-
-y <- train$flag_class
-x = rbind(train[,feat],test[,feat])
-x = as.matrix(x)
-x = matrix(as.numeric(x),nrow(x),ncol(x))
-
 # x = 1/(1+exp(-sqrt(x)))
 
 #############################
 ### Raw prediction ##########
 #############################
-    
-    # trind = 1:length(y)
-    # teind = (nrow(train)+1):nrow(x)
-    # 
-    # trainX = x[trind,]
-    # testX = x[teind,]
-    
-    # # Train the model
-    # bst = xgboost(data = cbind(train[baggedIndex,],bagPredictions), label = as.factor(y[baggedIndex]), 
-    #               max.depth = 6, eta = 0.15, nround = 500, min_child_weight = 4, colsample_bytree = 0.8,
-    #               nthread = 4, objective = "binary:logistic", verbose = 0, gamma = 0.1) 
-    # 
-    # # Make prediction
-    # pred = predict(bst,testX)
-    # pred = matrix(pred,9,length(pred)/9)
-    # pred = t(pred)
+dtrain <- xgb.DMatrix(as.matrix(train[,feat]), label = train$flag_class)
+dtest <- xgb.DMatrix(as.matrix(test[,feat]), label = test$flag_class)
+watchlist <- list(eval = dtest, train = dtrain)
+
+# # Train the model
+    bst <-
+        xgb.train(
+            data = dtrain, max.depth = 6, eta = 0.15, nround = 500, maximize = F, watchlist = watchlist, min_child_weight = 4, colsample_bytree = 0.8,
+            nthread = 4, objective = "binary:logistic", verbose = 1, print.every.n = 10, metrics = 'auc', num_parallel_tree = 1, gamma = 0.1
+        )
+
+# # Make prediction
+testPredictions = predict(bst,dtest)
 
 #################################
 ### Meta bagging Model ##########
 #################################
-testPredictions <- matrix(0, nrow = nrow(test), ncol = 1)
-bootRounds = 1:240
+# testPredictions <- matrix(0, nrow = nrow(test), ncol = 1)
+bootRounds = 1:10
 
 for (j in bootRounds) {
   print(j)
@@ -51,41 +41,43 @@ for (j in bootRounds) {
   OOBIndex = setdiff(1:nrow(train), baggedIndex)
   
   # Build the OOB model
-  OOBModel = randomForest(x=train[OOBIndex,feat], y=as.factor(y[OOBIndex]), replace=F, ntree=100, do.trace=T, mtry=7)
+  OOBModel = randomForest(x=train[OOBIndex,feat], y=as.factor(train$flag_class[OOBIndex]), replace=F, ntree=100, do.trace=T, mtry=7)
   bagPredictions = predict(OOBModel, train[baggedIndex,], type="prob")
   bagTestPredictions = predict(OOBModel, test, type="prob")
   
   # Build the Bag model
-  BagModel = xgboost(data = as.matrix(cbind(train[baggedIndex,feat],bagPredictions)), label = y[baggedIndex], 
-                     max.depth = 6, eta = 0.15, nround = 500, min_child_weight = 4, colsample_bytree = 0.8,
-                    nthread = 4, objective = "binary:logistic", verbose = 0, gamma = 0.1) 
+  dtrain <- xgb.DMatrix(as.matrix(train[baggedIndex,feat]), label = train$flag_class[baggedIndex])
+  dtest <- xgb.DMatrix(as.matrix(cbind(test[,feat],bagTestPredictions)), label = test$flag_class)
+  watchlist <- list(eval = dtest, train = dtrain)
+  
+  BagModel <- xgb.train(data = dtrain, max.depth = 6, eta = 0.15, nround = 500, watchlist = watchlist, #min_child_weight = 4, colsample_bytree = 0.8,
+                        nthread = 4, objective = "binary:logistic"#, metrics = 'auc', gamma = 0.1
+  )
   
   # Make test predictions and reshape them
-  tmpPredictions = predict(BagModel,as.matrix(cbind(test,bagTestPredictions)))
+  tmpPredictions = predict(BagModel,dtest)
   testPredictions = testPredictions + tmpPredictions
 }
-testPredictions = testPredictions/j
+testPredictions = testPredictions/(j+1)
 
-
-val <- validation
+#########################
+### Validation ##########
+#########################
+val <- test
 val$Y <- testPredictions
 tot_invest <- aggregate(INVEST ~ ACCOUNT_ID,data=val, sum, na.rm=T); names(tot_invest) <- c('ACCOUNT_ID', 'TOT_INVEST')
 val <- merge(val, tot_invest, all.x = TRUE, all.y = FALSE, by = c('ACCOUNT_ID'))
-val$INVEST_PERCENT <- val$INVEST/val$TOT_INVEST * val$Y#(val$Y - 0.5) * 2
-pred_fin <- aggregate(INVEST_PERCENT ~ ACCOUNT_ID, data=val, mean, na.rm=F)
+val$INVEST_PERCENT <- val$INVEST/val$TOT_INVEST * val$Y
+pred_fin <- aggregate(INVEST_PERCENT ~ ACCOUNT_ID, data=val, sum, na.rm=F)
 pred_fin2 <- aggregate(Y ~ ACCOUNT_ID, data=val, mean, na.rm=F)
-
-### Validation
 val_fin <- aggregate(flag_regr ~ ACCOUNT_ID, data=val, sum, na.rm=F)
 val_fin$PRED_PROFIT_LOSS_3 <- ifelse(val_fin$flag_regr > 0, 1, 0)
 
 ### Model Performance ###
-v <- merge(val_fin,pred_fin,all.x = TRUE,all.y = FALSE, by = 'ACCOUNT_ID')
-v <- merge(v,pred_fin2,all.x = TRUE,all.y = FALSE, by = 'ACCOUNT_ID')
-rocobj <- roc(v$PRED_PROFIT_LOSS_3, v[,4]);print(auc(rocobj)) # Invest * Possibility
+rocobj <- roc(val_fin$PRED_PROFIT_LOSS_3, pred_fin[,2]);print(auc(rocobj)) # Invest * Possibility
 print(auc(rocobj, partial.auc=c(1, .8), partial.auc.focus="se", partial.auc.correct=TRUE))
-rocobj <- roc(v$PRED_PROFIT_LOSS_3, v[,5]);print(auc(rocobj)) # Average Possibility
+rocobj <- roc(val_fin$PRED_PROFIT_LOSS_3, pred_fin2[,2]);print(auc(rocobj)) # Average Possibility
 print(auc(rocobj, partial.auc=c(1, .8), partial.auc.focus="se", partial.auc.correct=TRUE))
 
-prediction <- as.factor(ifelse(v$INVEST_PERCENT >=0.5, 1, 0))
-confusionMatrix(as.factor(v$PRED_PROFIT_LOSS_3), prediction)
+prediction <- as.factor(ifelse(pred_fin[,2] >=0.5, 1, 0))
+confusionMatrix(as.factor(val_fin$PRED_PROFIT_LOSS_3), prediction)
